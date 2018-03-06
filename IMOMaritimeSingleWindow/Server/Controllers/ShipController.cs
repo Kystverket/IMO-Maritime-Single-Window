99a8ac548c7ef669972e9da98772a5a4000bd48d
Axel Birger Kvistad <axel.b.kvistad@gmail.com>
2018-03-06 14:13:15 +0100

    Fix to dependency injection bug

--- IMOMaritimeSingleWindow/Server/Controllers/ShipController.cs
+++ IMOMaritimeSingleWindow/Server/Controllers/ShipController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using IMOMaritimeSingleWindow.Data;
 using IMOMaritimeSingleWindow.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
