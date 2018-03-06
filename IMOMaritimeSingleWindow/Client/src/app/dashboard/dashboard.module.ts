8c7e68d8fb6b045632e4f3525fb06882dc3bbc64
Gard Steinsvik <gardsteinsvik@gmail.com>
2018-03-06 09:56:25 +0100

    Moved all the registration components into 'basis-data' folder

--- IMOMaritimeSingleWindow/Client/src/app/dashboard/dashboard.module.ts
+++ IMOMaritimeSingleWindow/Client/src/app/dashboard/dashboard.module.ts
@@ -4,15 +4,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { EditUserComponent } from './user/edit-user/edit-user.component';
-import { RegistrerUserComponent } from './user/registrer-user/registrer-user.component';
-import { RegistrerShipComponent } from './ship/registrer-ship/registrer-ship.component';
-import { RegistrerLocationComponent } from './location/registrer-location/registrer-location.component';
-import { RegistrerCompanyComponent } from './company/registrer-company/registrer-company.component';
-import { UserFormComponent } from './user/user-form/user-form.component';
-import { ShipFormComponent } from './ship/ship-form/ship-form.component';
-import { LocationFormComponent } from './location/location-form/location-form.component';
-import { CompanyFormComponent } from './company/company-form/company-form.component';
+import { RegistrerUserComponent } from './basis-data/user/registrer-user/registrer-user.component';
+import { EditUserComponent } from './basis-data/user/edit-user/edit-user.component';
+import { RegistrerShipComponent } from './basis-data/ship/registrer-ship/registrer-ship.component';
+import { RegistrerLocationComponent } from './basis-data/location/registrer-location/registrer-location.component';
+import { RegistrerCompanyComponent } from './basis-data/company/registrer-company/registrer-company.component';
+import { UserFormComponent } from './basis-data/user/user-form/user-form.component';
+import { ShipFormComponent } from './basis-data/ship/ship-form/ship-form.component';
+import { LocationFormComponent } from './basis-data/location/location-form/location-form.component';
+import { CompanyFormComponent } from './basis-data/company/company-form/company-form.component';
 
 @NgModule({
   imports: [
