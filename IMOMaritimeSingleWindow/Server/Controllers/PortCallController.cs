085a4652b5f590bc43feb796b3651c5553f86821
Axel Birger Kvistad <axel.b.kvistad@gmail.com>
2018-03-15 18:04:43 +0100

    Simplified request error handling in port call controller

--- IMOMaritimeSingleWindow/Server/Controllers/PortCallController.cs
+++ IMOMaritimeSingleWindow/Server/Controllers/PortCallController.cs
@@ -29,33 +29,16 @@ namespace IMOMaritimeSingleWindow.Controllers
                 return BadRequest("Empty body");
             }
 
-            if (_context.Ship.Find(portCall.ShipId) == null)
+            try 
             {
-                return BadRequest("Ship with id " + portCall.ShipId + " does not exist.");
-            }
-
-            if (_context.Location.Find(portCall.LocationId) == null)
-            {
-                return BadRequest("Location with ID " + portCall.LocationId + " does not exist.");
-            }
-
-            if (_context.Location.Find(portCall.NextLocationId) == null)
-            {
-                return BadRequest("(NextLocation) Location with ID " + portCall.NextLocationId + " does not exist.");
-            }
-
-            if (_context.Location.Find(portCall.PreviousLocationId) == null)
-            {
-                return BadRequest("(PreviousLocation) Location with ID " + portCall.PreviousLocationId + " does not exist.");
-            }
-
-            if (_context.PortCallStatus.Find(portCall.PortCallStatusId) == null)
-            {
-                return BadRequest("PortCallStatus with ID " + portCall.PortCallStatusId + " does not exist.");
-            }
-
             _context.PortCall.Add(portCall);
-            _context.SaveChanges();
+            _context.SaveChanges();                              
+            } catch (DbUpdateException ex) when (ex.InnerException is Npgsql.PostgresException)
+            {
+                Npgsql.PostgresException innerEx = (Npgsql.PostgresException) ex.InnerException;
+                return BadRequest("PostgreSQL Error Code: " + innerEx.SqlState);
+            }
+            
             return Json(portCall);
         }
 
