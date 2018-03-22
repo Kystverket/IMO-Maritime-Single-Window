99b46df54632a1e1f0b5f8c8068a4999ac94b3ee
Axel Birger Kvistad <axel.b.kvistad@gmail.com>
2018-03-22 15:19:35 +0100

    Routing and styling fixes

--- IMOMaritimeSingleWindow/Client/src/app/shared/models/user-model.ts
+++ IMOMaritimeSingleWindow/Client/src/app/shared/models/user-model.ts
@@ -1,4 +1,4 @@
-export class User {
+export class UserModel {
     userId: number;
     userUuid: number;
     userName: string;
---
+++ IMOMaritimeSingleWindow/Client/src/app/shared/services/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { of } from "rxjs/observable/of";
+import { BehaviorSubject, Subject } from 'rxjs';
+import { UserModel } from "../models/user-model";
+
+@Injectable()
+export class UserService {
+    constructor(private http: Http) {
+        this.actionUrl = 'api/user/search';
+        this.registerUserUrl = 'api/user/register';
+    }
+
+    private actionUrl: string;
+    private registerUserUrl: string;
+
+    // private companyDataSource = new BehaviorSubject<any>(null);
+    // companyData$ = this.companyDataSource.asObservable();
+
+    // private countryDataSource = new BehaviorSubject<any>(null);
+    // countryData$ = this.countryDataSource.asObservable();
+
+    // private userFlagCodeDataSource = new BehaviorSubject<any>(null);
+    // userFlagCodeData$ = this.userFlagCodeDataSource.asObservable();
+
+    registerUser(newUser: any) {
+        return this.http.post(this.registerUserUrl, newUser)
+                .map(res => res.json());
+    }    
+    
+}
