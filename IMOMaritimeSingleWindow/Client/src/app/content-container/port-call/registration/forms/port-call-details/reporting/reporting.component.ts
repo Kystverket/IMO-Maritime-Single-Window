64884a14ba6dc5aa205f89754ca23462635f7971
Gard Steinsvik <gardsteinsvik@gmail.com>
2018-03-23 09:43:03 +0100

    Added new icons for cargo and clipboard, moved the checkboxes in reporting to the left

--- IMOMaritimeSingleWindow/Client/src/app/content-container/port-call/registration/forms/port-call-details/reporting/reporting.component.ts
+++ IMOMaritimeSingleWindow/Client/src/app/content-container/port-call/registration/forms/port-call-details/reporting/reporting.component.ts
@@ -10,20 +10,18 @@ export class ReportingComponent implements OnInit {
   baseIconUrl: string = "assets/images/VoyageIcons/128x128/";
   checkboxes: any = [
     {name: "Hazmat",  icon: this.baseIconUrl+"hazard.png", checked: false},
-    {name: "Bunkers", icon: this.baseIconUrl+"barrel.png", checked: false}
+    {name: "Bunkers", icon: this.baseIconUrl+"barrel.png", checked: false},
+    {name: "Cargo",   icon: this.baseIconUrl+"cargo.png",  checked: false}
   ];
 
   constructor() { }
 
   checkboxChecked(checkboxModel) {
     checkboxModel.checked = !checkboxModel.checked;
-    console.log(checkboxModel.name + ' ' + checkboxModel.checked);
-    
-    
-    
   }
 
   ngOnInit() {
+
   }
 
 }
