58a93101fb08b77a6f86664a608c9041861284a0
Gard Steinsvik <gardsteinsvik@gmail.com>
2018-04-02 13:52:35 +0200

    Added cargo component for declaring the cargo weight in port call details

---
+++ IMOMaritimeSingleWindow/Client/src/app/content-container/port-call/registration/forms/port-call-details/cargo/cargo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cargo',
+  templateUrl: './cargo.component.html',
+  styleUrls: ['./cargo.component.css']
+})
+export class CargoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
