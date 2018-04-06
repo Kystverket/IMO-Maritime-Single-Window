5911d25baf9c58c06efa37116223c3e0650ce2f6
Gard Steinsvik <gardsteinsvik@gmail.com>
2018-04-06 10:40:25 +0200

    Fixed potential null pointer errors in info component. Fixed ship length not displaying in find ship component

--- IMOMaritimeSingleWindow/Client/src/app/content-container/port-call/registration/info/info.component.ts
+++ IMOMaritimeSingleWindow/Client/src/app/content-container/port-call/registration/info/info.component.ts
@@ -33,21 +33,26 @@ export class InfoComponent implements OnInit {
     this.portCallService.shipData$.subscribe(
       data => {
         if (data != null) {
-          console.log(data);
-          this.shipFlag = data.country.twoCharCode;
+          if (data.country != null) {
+            this.shipFlag = data.country.twoCharCode;
+          }
           this.portCallShipInfo.find(p => p.description == "Ship Name:").data = data.ship.name;
           this.portCallShipInfo.find(p => p.description == "Call Sign:").data = data.ship.callSign;
           this.portCallShipInfo.find(p => p.description == "IMO no:").data = data.ship.imoNo;
           this.portCallShipInfo.find(p => p.description == "Gross Tonnage:").data = data.ship.grossTonnage;
           this.portCallShipInfo.find(p => p.description == "Length:").data = data.ship.length;
-          this.portCallShipInfo.find(p => p.description == "Ship Type:").data = data.shipType;
+          if (data.shipType != null) {
+            this.portCallShipInfo.find(p => p.description == "Ship Type:").data = data.shipType.name;
+          }
         }
       }
     );
     this.portCallService.locationData$.subscribe(
       data => {
-        if (data != null) {
-          this.locationFlag = data.country.twoCharCode;
+        if (data != null) {          
+          if (data.country != null) {
+            this.locationFlag = data.country.twoCharCode;
+          }
           this.portCallLocationInfo.find(p => p.description == "Location:").data = data.name;
           this.portCallLocationInfo.find(p => p.description == "Location Code:").data = data.locationCode;
         }
@@ -60,7 +65,7 @@ export class InfoComponent implements OnInit {
             + " " + this.dateTimeFormat(data.eta.hour) + ":" + this.dateTimeFormat(data.eta.minute);
           let etd = data.etd.year + "-" + this.dateTimeFormat(data.etd.month) + "-" + this.dateTimeFormat(data.etd.day)
             + " " + this.dateTimeFormat(data.etd.hour) + ":" + this.dateTimeFormat(data.etd.minute);
-            
+
           this.portCallLocationInfo.find(p => p.description == "ETA:").data = eta;
           this.portCallLocationInfo.find(p => p.description == "ETD:").data = etd;
         }
