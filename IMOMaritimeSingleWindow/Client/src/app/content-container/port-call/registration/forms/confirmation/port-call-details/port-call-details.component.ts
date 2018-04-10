49ab7f1a712df917790dc0bd4bd618874150dbb3
Gard Steinsvik <gardsteinsvik@gmail.com>
2018-04-10 15:12:18 +0200

    Loading port call detail names from code

--- IMOMaritimeSingleWindow/Client/src/app/content-container/port-call/registration/forms/confirmation/port-call-details/port-call-details.component.ts
+++ IMOMaritimeSingleWindow/Client/src/app/content-container/port-call/registration/forms/confirmation/port-call-details/port-call-details.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { PortCallService } from '../../../../../../shared/services/port-call.service';
+
+const NO_OF_CREW = "No. of Crew";
+const NO_OF_PASSENGERS = "No. of Passengers";
+const ACTUAL_DRAUGHT = "Actual Draught";
+const AIR_DRAUGHT = "Air Draught";
+const GROSS_GROSS_WEIGHT = "Gross Gross Weight";
+const GROSS_WEIGHT = "Gross Weight";
 
 @Component({
   selector: 'app-port-call-details',
@@ -7,7 +15,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortCallDetailsComponent implements OnInit {
 
-  constructor() { }
+  portCallDetailsInfo: any[] = [
+    { description: NO_OF_CREW, data: null },
+    { description: NO_OF_PASSENGERS, data: null },
+    { description: ACTUAL_DRAUGHT, data: null },
+    { description: AIR_DRAUGHT, data: null },
+    { description: GROSS_GROSS_WEIGHT, data: null },
+    { description: GROSS_WEIGHT, data: null }
+  ];
+
+  constructor(private portCallService: PortCallService) { }
 
   ngOnInit() {
     
