ddf81f959085fb48979025ea76a91c21cf6b0a46
Joar K. Mauseth <jkm99412@gmail.com>
2018-04-20 12:36:51 +0200

    ViewModel validation registeruser

--- IMOMaritimeSingleWindow/Server/ViewModels/RegistrationViewModel.cs
+++ IMOMaritimeSingleWindow/Server/ViewModels/RegistrationViewModel.cs
@@ -1,10 +1,18 @@
 
 
+using System.ComponentModel.DataAnnotations;
+
 namespace IMOMaritimeSingleWindow.ViewModels
 {
     public class RegistrationViewModel
     {
+        [EmailAddress]
         public string Email { get; set; }
+        
+        [Phone]
+        [StringLength(maximumLength:11, MinimumLength = 8)]
+        public string PhoneNumber { get; set; }
+        
         public string FirstName { get; set; }
         public string LastName { get; set; }
     }
--- IMOMaritimeSingleWindow/Server/ViewModels/Validations/RegistrationViewModelValidator.cs
+++ IMOMaritimeSingleWindow/Server/ViewModels/Validations/RegistrationViewModelValidator.cs
@@ -9,6 +9,7 @@ namespace IMOMaritimeSingleWindow.ViewModels.Validations
         public RegistrationViewModelValidator()
         {
             RuleFor(vm => vm.Email).NotEmpty().WithMessage("Email cannot be empty");
+            RuleFor(vm => vm.PhoneNumber).NotEmpty().WithMessage("PhoneNumber cannot be empty");
             RuleFor(vm => vm.FirstName).NotEmpty().WithMessage("FirstName cannot be empty");
             RuleFor(vm => vm.LastName).NotEmpty().WithMessage("LastName cannot be empty");
         }
