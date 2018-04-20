b043b7fc49c8ccc8a292cc419040045f1e34ba30
Joar K. Mauseth <jkm99412@gmail.com>
2018-04-20 12:36:26 +0200

    SOme table renaming

--- IMOMaritimeSingleWindow/Server/Data/UserDbContext.cs
+++ IMOMaritimeSingleWindow/Server/Data/UserDbContext.cs
@@ -4,6 +4,7 @@ using IMOMaritimeSingleWindow.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using IMOMaritimeSingleWindow.Extensions;
 
 namespace IMOMaritimeSingleWindow.Data
 {
@@ -23,31 +24,67 @@ namespace IMOMaritimeSingleWindow.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>().ToTable("ApplicationUser");
+            builder.Entity<ApplicationRole>().ToTable("ApplicationRole");
+            builder.Entity<ApplicationUserRole>().ToTable("ApplicationUserRole");
+            builder.Entity<ApplicationUserRole>().Property(ur => ur.RoleId).HasColumnName("ApplicationRoleId");
+            builder.Entity<ApplicationUserRole>().Property(ur => ur.UserId).HasColumnName("ApplicationUserId");
+
+            builder.Entity<ApplicationUserToken>().ToTable("ApplicationUserToken");
+            builder.Entity<ApplicationUserLogin>().ToTable("ApplicationUserLogin");
+            builder.Entity<ApplicationRoleClaim>().ToTable("ApplicationRoleRight");
+            builder.Entity<ApplicationUserClaim>().ToTable("ApplicationUserRight"); //To be coupled through role?
+
+            builder.Entity<ApplicationUser>().Ignore(entity => entity.PasswordHash);
+
+            foreach (var entity in builder.Model.GetEntityTypes())
+            {
+                // Replace table names
+                entity.Relational().TableName = entity.Relational().TableName.ToSnakeCase();
+
+                // Replace column names            
+                foreach (var property in entity.GetProperties())
+                {
+                    property.Relational().ColumnName = property.Name.ToSnakeCase();
+                }
+
+                foreach (var key in entity.GetKeys())
+                {
+                    key.Relational().Name = key.Relational().Name.ToSnakeCase();
+                }
+
+                foreach (var key in entity.GetForeignKeys())
+                {
+                    key.Relational().Name = key.Relational().Name.ToSnakeCase();
+                }
+
+                foreach (var index in entity.GetIndexes())
+                {
+                    index.Relational().Name = index.Relational().Name.ToSnakeCase();
+                }
+            }
+
+
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
 
-            /**
-            builder.Entity<ApplicationUser>(entity => entity.ToTable(name: "ApplicationUser" );
-            builder.Entity<ApplicationRole>(entity => entity.ToTable(name: "ApplicationRole"));
-            builder.Entity<ApplicationUserRole>(entity => entity.ToTable(name: "ApplicationUserRole"));
-            builder.Entity<ApplicationUserToken>(entity => entity.ToTable(name: "ApplicationUserToken"));
-            builder.Entity<ApplicationUserLogin>(entity => entity.ToTable(name: "ApplicationUserLogin"));
-            builder.Entity<ApplicationRoleClaim>(entity => entity.ToTable(name: "ApplicationRoleClaim"));
-            builder.Entity<ApplicationUserClaim>(entity => entity.ToTable(name: "ApplicationUserClaim"));
-            */
 
-            /**
-            builder.Entity<ApplicationUser>().ToTable("Person");
-            builder.Entity<ApplicationRole>().ToTable("Role");
-            builder.Entity<ApplicationUserRole>(entity => entity.ToTable(name: "PersonRole"));
-            builder.Entity<ApplicationUserToken>(entity => entity.ToTable(name: "PersonToken"));
-            builder.Entity<ApplicationUserLogin>(entity => entity.ToTable(name: "PersonLogin"));
-            builder.Entity<ApplicationRoleClaim>(entity => entity.ToTable(name: "RoleRight"));
-            builder.Entity<ApplicationUserClaim>(entity => entity.ToTable(name: "PersonRight")); //To be coupled through role?
-            */
+            //builder.Entity<ApplicationUser>(entity => entity.ToTable(name: "ApplicationUser" ));
+            //builder.Entity<ApplicationRole>(entity => entity.ToTable(name: "ApplicationRole"));
+            //builder.Entity<ApplicationUserRole>(entity => entity.ToTable(name: "ApplicationUserRole"));
+            //builder.Entity<ApplicationUserToken>(entity => entity.ToTable(name: "ApplicationUserToken"));
+            //builder.Entity<ApplicationUserLogin>(entity => entity.ToTable(name: "ApplicationUserLogin"));
+            //builder.Entity<ApplicationRoleClaim>(entity => entity.ToTable(name: "ApplicationRoleClaim"));
+            //builder.Entity<ApplicationUserClaim>(entity => entity.ToTable(name: "ApplicationUserClaim"));
+
+
 
             
+            
+
+            /*
             builder.Entity<ApplicationUser>().ToTable(nameof(ApplicationUser));
             builder.Entity<ApplicationRole>().ToTable(nameof(ApplicationRole));
             builder.Entity<ApplicationUserRole>().ToTable(nameof(ApplicationUserRole));
@@ -55,6 +92,7 @@ namespace IMOMaritimeSingleWindow.Data
             builder.Entity<ApplicationUserLogin>().ToTable(nameof(ApplicationUserLogin));
             builder.Entity<ApplicationRoleClaim>().ToTable(nameof(ApplicationRoleClaim));
             builder.Entity<ApplicationUserClaim>().ToTable(nameof(ApplicationUserClaim)); //To be coupled through role?
+            */
 
             builder.Entity<Password>().Property(p => p.PasswordId).UseNpgsqlSerialColumn();
                 
--- IMOMaritimeSingleWindow/Server/Identity/Stores/ApplicationUserStore.cs
+++ IMOMaritimeSingleWindow/Server/Identity/Stores/ApplicationUserStore.cs
@@ -8,19 +8,30 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using IMOMaritimeSingleWindow.Data;
 using System.Threading;
 
+using IMOMaritimeSingleWindow.TestModels;
+
 namespace IMOMaritimeSingleWindow.Identity
 {
-    public class ApplicationUserStore : UserStore<ApplicationUser, ApplicationRole, UserDbContext, Guid,
-        ApplicationUserClaim, ApplicationUserRole, ApplicationUserLogin, ApplicationUserToken, ApplicationRoleClaim>,
+    public class ApplicationUserStore : UserStore<ApplicationUser, ApplicationRole, usertestContext, Guid,
+        AspNetUserClaims, AspNetUserRoles, AspNetUserLogins, AspNetUserTokens, AspNetRoleClaims>,
         IUserPasswordStore<ApplicationUser>
 
     {
-        public ApplicationUserStore(UserDbContext context)
+        public ApplicationUserStore(usertestContext context)
             : base(context)
         {
         }
 
-        /*
+        
+
+        public override Task<IdentityResult> CreateAsync(ApplicationUser user, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return base.CreateAsync(user, cancellationToken);
+        }
+
+       
+
+
         public override Task<string> GetPasswordHashAsync(ApplicationUser user, CancellationToken cancellationToken = default(CancellationToken))
         {
             var passwordHash = Context.Password
@@ -33,10 +44,11 @@ namespace IMOMaritimeSingleWindow.Identity
         public override async Task SetPasswordHashAsync(ApplicationUser user, string passwordHash, CancellationToken cancellationToken = default(CancellationToken))
         {
             await base.SetPasswordHashAsync(user, passwordHash, cancellationToken);
-            await Context.Password.AddAsync(new Password { IdentityId = user.Id, PasswordHash = passwordHash });
-            await Context.SaveChangesAsync();
+            
+            //await Context.Password.AddAsync(new Password { IdentityId = user.Id, PasswordHash = passwordHash });
+            //await Context.SaveChangesAsync();
         }
-        */
+        
         
     }
 }
