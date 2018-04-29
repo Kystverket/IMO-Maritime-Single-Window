90086f932e3c4fafe7431b0e5554f89a1921f05d
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2018-04-29 22:08:49 +0200

    Only build the angular frontend and publish it to the wwwroot folder.

--- BuildFrontend.bat
+++ BuildFrontend.bat
@@ -1,2 +1,6 @@
-dotnet publish ./IMOMaritimeSingleWindow\Server\IMOMaritimeSingleWindow.csproj --runtime ubuntu-x64
-dotnet publish ./IMOMaritimeSingleWindow\Server\IMOMaritimeSingleWindow.csproj --runtime win10-x64
+rem call dotnet publish ./IMOMaritimeSingleWindow\Server\IMOMaritimeSingleWindow.csproj --runtime ubuntu-x64
+rem call dotnet publish ./IMOMaritimeSingleWindow\Server\IMOMaritimeSingleWindow.csproj --runtime win10-x64
+cd ./IMOMaritimeSingleWindow/Client
+call ng build --bundle-dependencies all --output-path ./../Server/wwwroot
+rem move wwwroot ./../Server/wwwroot
+cd ./../..
