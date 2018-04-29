e54bad46f59cab618e0344cb83df87c83d5d2c3a
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2018-04-29 21:17:13 +0200

    Added basic build script that publishes the MVC app to win10-x64 and ubuntu-x64.

---
+++ BuildAndPublish.bat
@@ -0,0 +1,2 @@
+dotnet publish ./IMOMaritimeSingleWindow\Server\IMOMaritimeSingleWindow.csproj --runtime ubuntu-x64
+dotnet publish ./IMOMaritimeSingleWindow\Server\IMOMaritimeSingleWindow.csproj --runtime win10-x64
