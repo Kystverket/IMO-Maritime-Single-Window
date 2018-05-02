674672a03939b6d0f3135d5469e984204a6cc39c
Axel Kvistad <axel.b.kvistad@gmail.com>
2018-05-02 12:47:50 +0200

    Added registration to location service, started on component and html

--- IMOMaritimeSingleWindow/Client/src/app/main-content/content-container/basis-data/location/register-location/register-location.component.ts
+++ IMOMaritimeSingleWindow/Client/src/app/main-content/content-container/basis-data/location/register-location/register-location.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { LocationModel } from '../../../../../shared/models/location-model';
+import { LocationService } from '../../../../../shared/services/location.service';
 
 @Component({
   selector: 'app-register-location',
   templateUrl: './register-location.component.html',
-  styleUrls: ['./register-location.component.css']
+  styleUrls: ['./register-location.component.css'],
+  providers: [LocationModel, LocationService]
 })
 export class RegisterLocationComponent implements OnInit {
+  locationTypeList: any[];
+  locationTypeSelected: boolean;
+  selectedLocationType: any;
+  locationTypeDropdownString: string = "Select location type";
 
-  constructor() { }
+  constructor(public locationModel: LocationModel, private locationService: LocationService) { }
 
   ngOnInit() {
+    this.locationService.getLocationTypes().subscribe(
+      locationTypesData => {
+        this.locationTypeList = locationTypesData;
+      }
+    );
   }
 
 }
--- IMOMaritimeSingleWindow/Client/src/app/shared/services/location.service.ts
+++ IMOMaritimeSingleWindow/Client/src/app/shared/services/location.service.ts
@@ -8,15 +8,26 @@ export class LocationService {
     constructor(private http: Http) {
         this.searchService = new SearchService(http);
         this.searchUrl = 'api/location/search/';
+        this.registerLocationUrl = 'api/location/register';
         this.getLocationTypesUrl = 'api/locationtype/getall';
     }
 
     private searchService: SearchService;
     private searchUrl: string;
+    private registerLocationUrl: string;
     private getLocationTypesUrl: string;
 
     public search(term: string) {
         return this.searchService.search(this.searchUrl, term);
+    }
+
+    public registerLocation(newLocation: LocationModel) {
+        return this.http.post(this.registerLocationUrl, newLocation)
+                .map(res => res.json()).subscribe(
+                    locData => {
+                        console.log(locData);
+                    }
+                );
     }
 
     public getLocationTypes() {
