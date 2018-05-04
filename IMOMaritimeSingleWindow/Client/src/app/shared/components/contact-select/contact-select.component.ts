bf774198a0a4af7a0e1827beb315667adbcfd8c7
Gard Steinsvik <gardsteinsvik@gmail.com>
2018-05-04 14:23:18 +0200

    Added component for selecting multiple contact mediums, and a linked input field will appear for each selected contact medium.

--- IMOMaritimeSingleWindow/Client/src/app/shared/components/contact-select/contact-select.component.ts
+++ IMOMaritimeSingleWindow/Client/src/app/shared/components/contact-select/contact-select.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactSelectComponent implements OnInit {
 
+  contactDataList: any = [
+    { name: 'E-mail', value: '' },
+    { name: 'Inmarsat', value: '' }
+  ];
+  selectedContactMediums = [];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  contactMediumSelected() {
+    console.log(this.selectedContactMediums);
+    
+  }
+
+  contactInfoChanged(contactMedium: any, $event: any) {
+    console.log(contactMedium);
+    console.log($event);    
+  }
 }
--- IMOMaritimeSingleWindow/Client/src/app/shared/shared.module.ts
+++ IMOMaritimeSingleWindow/Client/src/app/shared/shared.module.ts
@@ -1,20 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
 import { ClearancesComponent } from './components/confirmation-view/clearances/clearances.component';
 import { ConfirmationViewComponent } from './components/confirmation-view/confirmation-view.component';
 import { PortCallDetailsComponent } from './components/confirmation-view/port-call-details/port-call-details.component';
 import { SelectedPurposesComponent } from './components/confirmation-view/port-call-details/selected-purposes/selected-purposes.component';
+import { ContactSelectComponent } from './components/contact-select/contact-select.component';
 import { SsnCardComponent } from './components/ssn-card/ssn-card.component';
 import { TableCardComponent } from './components/table-card/table-card.component';
-import { PositiveNumberValidator } from './utils/custom-validators/positive-number-validator.directive';
-import { FormsModule } from '@angular/forms';
-import { NumberValidator } from './utils/custom-validators/number-validator.directive';
 import { IntegerValidator } from './utils/custom-validators/integer-validator.directive';
-import { ContactSelectComponent } from './components/contact-select/contact-select.component';
+import { NumberValidator } from './utils/custom-validators/number-validator.directive';
+import { PositiveNumberValidator } from './utils/custom-validators/positive-number-validator.directive';
 
 @NgModule({
   imports: [
-    CommonModule, FormsModule
+    CommonModule, FormsModule, NgSelectModule
   ],
   declarations: [
     SsnCardComponent,
