03034674e322b4eea1d385ee90f4beb98fa977e0
Gard Steinsvik <gardsteinsvik@gmail.com>
2018-05-04 17:01:17 +0200

    Implemented selecting preferred contact medium. Using models instead of any.

--- IMOMaritimeSingleWindow/Client/src/app/shared/components/contact-select/contact-select.component.ts
+++ IMOMaritimeSingleWindow/Client/src/app/shared/components/contact-select/contact-select.component.ts
@@ -1,30 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { ConstantsService } from '../../services/constants.service';
+import { ContactMediumModel } from '../../models/contact-medium-model';
+import { ContactModel } from '../../models/contact-model';
 
 @Component({
   selector: 'app-contact-select',
   templateUrl: './contact-select.component.html',
-  styleUrls: ['./contact-select.component.css']
+  styleUrls: ['./contact-select.component.css'],
+  providers: [ConstantsService]
 })
 export class ContactSelectComponent implements OnInit {
 
-  contactDataList: any = [
-    { name: 'E-mail', value: '' },
-    { name: 'Inmarsat', value: '' }
-  ];
-  selectedContactMediums = [];
+  contactList: ContactModel[];
 
-  constructor() { }
+  selectedContactModels: ContactModel[];
+
+  constructor(private constantsService: ConstantsService) { }
 
   ngOnInit() {
+    this.constantsService.getContactMediumList().subscribe(
+      data => {
+        console.log(data);
+
+        if (data) this.contactList = data.map(d => {
+          let contactModel = new ContactModel();
+          contactModel.contactMedium = d;
+          console.log(contactModel);
+
+          return contactModel;
+        });
+      }
+    );
   }
 
   contactMediumSelected() {
-    console.log(this.selectedContactMediums);
-    
+    console.log(this.selectedContactModels);
+
   }
 
-  contactInfoChanged(contactMedium: any, $event: any) {
+  preferredSet(selectedContactModel: ContactModel) {    
+    let newUpdatedContactMediums = this.selectedContactModels.map(
+      oldContactModel => {
+        if (oldContactModel.contactMedium.contactMediumId === selectedContactModel.contactMedium.contactMediumId) return selectedContactModel;
+        let updatedContactModel = oldContactModel;
+        updatedContactModel.isPreferred = false;
+        return updatedContactModel
+      }
+    );
+  }
+
+  contactInfoChanged(contactMedium: ContactModel, $event: any) {
     console.log(contactMedium);
-    console.log($event);    
+    console.log($event);
   }
 }
