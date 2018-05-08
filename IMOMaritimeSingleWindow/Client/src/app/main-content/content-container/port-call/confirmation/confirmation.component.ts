526330167e417e59be8d5b3b3e1ff816b1848db2
Gard Steinsvik <gardsteinsvik@gmail.com>
2018-05-08 15:53:57 +0200

    Fixed issue where selected FAL forms were not displayed in port call confirmation view

--- IMOMaritimeSingleWindow/Client/src/app/main-content/content-container/port-call/confirmation/confirmation.component.ts
+++ IMOMaritimeSingleWindow/Client/src/app/main-content/content-container/port-call/confirmation/confirmation.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { PortCallService } from '../../../../shared/services/port-call.service';
 
 @Component({
   selector: 'app-confirmation',
@@ -8,24 +7,7 @@ import { PortCallService } from '../../../../shared/services/port-call.service';
 })
 export class ConfirmationComponent implements OnInit {
 
-  iconPath = "assets/images/VoyageIcons/128x128/white/";
-  falForms: any;
+  constructor() { }
 
-  constructor(private portCallService: PortCallService) { }
-
-  ngOnInit() {
-    this.portCallService.reportingForThisPortCallData$.subscribe((reportingData) => {
-      if (reportingData != null) {
-        this.falForms = [
-          { name: "Hazmat", icon: "hazard.png", checked: reportingData.reportingHazmat || false },
-          { name: "Bunkers", icon: "barrel.png", checked: reportingData.reportingBunkers || false },
-          { name: "Cargo", icon: "cargo.png", checked: reportingData.reportingCargo || false },
-          { name: "Ship Stores", icon: "alcohol.png", checked: reportingData.reportingShipStores || false },
-          { name: "Crew", icon: "crew.png", checked: reportingData.reportingCrew || false },
-          { name: "Pax", icon: "pax.png", checked: reportingData.reportingPax || false },
-          { name: "Waste", icon: "trash.png", checked: reportingData.reportingWaste || false }
-        ];
-      }
-    });
-  }
+  ngOnInit() { }
 }
