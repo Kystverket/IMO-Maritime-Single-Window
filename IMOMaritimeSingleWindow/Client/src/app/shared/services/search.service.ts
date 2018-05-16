cc1806727b49896e1dcd269861c202838b544f76
Axel Kvistad <axel.b.kvistad@gmail.com>
2018-05-16 16:50:38 +0200

    Fixed issues with flag code and organization search in register-ship

--- IMOMaritimeSingleWindow/Client/src/app/shared/services/search.service.ts
+++ IMOMaritimeSingleWindow/Client/src/app/shared/services/search.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
-import { of } from "rxjs/observable/of";
 
 @Injectable()
 export class SearchService {
@@ -11,10 +10,6 @@ export class SearchService {
 
         let encoded_term: string = encodeURIComponent(term);
         let uri: string = [baseUrl, encoded_term].join('/');
-
-        if (term === '' || term.length < 2) {
-            return of([]);
-        }
 
         return this.http.get(uri).map(res => res.json());
     }
