d09ab0580ad3ca6fc842267072e3f3e440a1144d
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2018-05-21 23:04:05 +0200

    Fix frontend build script. Didn't call npm install, leading to potentially missing dependencies.

--- BuildFrontend.bat
+++ BuildFrontend.bat
@@ -1,6 +1,7 @@
 rem call dotnet publish ./IMOMaritimeSingleWindow\Server\IMOMaritimeSingleWindow.csproj --runtime ubuntu-x64
 rem call dotnet publish ./IMOMaritimeSingleWindow\Server\IMOMaritimeSingleWindow.csproj --runtime win10-x64
 cd ./IMOMaritimeSingleWindow/Client
+call npm install
 call ng build --bundle-dependencies all --output-path ./../Server/wwwroot
 rem move wwwroot ./../Server/wwwroot
 cd ./../..
