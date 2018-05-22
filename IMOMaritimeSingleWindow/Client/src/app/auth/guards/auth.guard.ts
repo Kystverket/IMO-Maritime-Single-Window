705ae857e7732d25e23863ac844b13a1a6908670
Joar K. Mauseth <jkm99412@gmail.com>
2018-05-23 00:11:17 +0200

    Also check whether token is present in localStorage

--- IMOMaritimeSingleWindow/Client/src/app/auth/guards/auth.guard.ts
+++ IMOMaritimeSingleWindow/Client/src/app/auth/guards/auth.guard.ts
@@ -2,28 +2,36 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../../shared/services/auth-service';
+import { LoginService } from '../../shared/services/login.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
   constructor(
     private router: Router,
-    private authService: AuthService
-  )
+    private authService: AuthService,
+    private loginService: LoginService  )
     {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 
-      return this.authService.hasValidToken()
-      .map(tokenValid => {
-        if(!tokenValid) {
-          this.router.navigate(['/login']);
-          return false;
-        } else {
-          return true;
-        }
-      })
+      if(!this.authService.hasToken()) {
+        this.router.navigate(['/login']);
+        return false;
+      } else {
+        return this.authService.hasValidToken()
+        .map(tokenValid => {
+          if(!tokenValid) {
+            this.loginService.logout();
+            this.router.navigate(['/login']);
+            return false;
+          } else {
+            return true;
+          }
+        })
+      }
+      
   }
 }
--- IMOMaritimeSingleWindow/Client/src/app/auth/guards/login-auth.guard.ts
+++ IMOMaritimeSingleWindow/Client/src/app/auth/guards/login-auth.guard.ts
@@ -2,27 +2,38 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../../shared/services/auth-service';
+import { LoginService } from '../../shared/services/login.service';
 
 @Injectable()
 export class LoginAuthGuard implements CanActivate {
 
   constructor(
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private loginService: LoginService
   ) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
       
-      return this.authService.hasValidToken()
-      .map(tokenValid => {
-        if(tokenValid) {
-          this.router.navigate(['']);
-          return false;
-        } else {
-          return true;
-        }
-      })
+      if(!this.authService.hasToken())
+        return true;
+      else {
+        return this.authService.hasValidToken()
+        .map(tokenValid => {
+          if(tokenValid) {
+            this.router.navigate(['']);
+            return false;
+          } else {
+            /* User tries to access login route with an invalid token
+             * This is OK, just remove relevant variables from localStorage
+             * so user can receive a new token upon login. */
+            this.loginService.logout();
+            return true;
+          }
+        })
+      }
+      
   }
 }
