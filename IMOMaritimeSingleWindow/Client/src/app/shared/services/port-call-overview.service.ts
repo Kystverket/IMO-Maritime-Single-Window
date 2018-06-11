39e21b954b44a8f9a8af6dc108e145684d69134c
Gard Steinsvik <gardsteinsvik@gmail.com>
2018-06-11 12:08:49 +0200

    Reduced the action button size to make the port call list more compact.

--- IMOMaritimeSingleWindow/Client/src/app/shared/services/port-call-overview.service.ts
+++ IMOMaritimeSingleWindow/Client/src/app/shared/services/port-call-overview.service.ts
@@ -1,49 +1,54 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { BehaviorSubject } from 'rxjs';
 import { BaseRequest } from '../utils/base.request';
 import { ConfigService } from '../utils/config.service';
 import { AuthRequest } from './auth.request.service';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class PortCallOverviewService extends BaseRequest {
-    constructor(private http: Http,
-        authRequestService: AuthRequest,
-        configService: ConfigService) {
-        super(configService, authRequestService);
-        this.portCallUrl = "api/portcall";
-        this.overviewUrl = this.portCallUrl + "/overview";
-        this.portCallUserUrl = this.portCallUrl + "/user";
-    }
     private portCallUrl: string;
     private portCallUserUrl: string;
     private overviewUrl: string;
 
     private overviewDataSource = new BehaviorSubject<any>(null);
     overviewData$ = this.overviewDataSource.asObservable();
-    setOverviewData(data) {
-        this.overviewDataSource.next(data);
-    }
 
     private draftOverviewDataSource = new BehaviorSubject<any>(null);
     draftOverviewData$ = this.draftOverviewDataSource.asObservable();
-    setDraftData(data) {
-        this.draftOverviewDataSource.next(data);
-    }
 
     private clearedByUserAgencyDataSource = new BehaviorSubject<any>(null);
     clearedByUserAgencyOverviewData$ = this.clearedByUserAgencyDataSource.asObservable();
-    setClearedData(data) {
-        this.clearedByUserAgencyDataSource.next(data);
-    }
 
     private portCallDataSource = new BehaviorSubject<any>(null);
     portCallData$ = this.portCallDataSource.asObservable();
 
+    constructor(
+        private http: Http,
+        authRequestService: AuthRequest,
+        configService: ConfigService
+    ) {
+        super(configService, authRequestService);
+        this.portCallUrl = 'api/portcall';
+        this.overviewUrl = this.portCallUrl + '/overview';
+        this.portCallUserUrl = this.portCallUrl + '/user';
+    }
+
+    setOverviewData(data) {
+        this.overviewDataSource.next(data);
+    }
+
+    setDraftData(data) {
+        this.draftOverviewDataSource.next(data);
+    }
+
+    setClearedData(data) {
+        this.clearedByUserAgencyDataSource.next(data);
+    }
+
     getOverview(portCallId: number) {
-        let uri: string = [this.overviewUrl, portCallId].join('/');
-        return this.http.get(uri)
-            .map(res => res.json());
+        const uri: string = [this.overviewUrl, portCallId].join('/');
+        return this.http.get(uri).map(res => res.json());
     }
 
     setPortCallData(data) {
@@ -51,15 +56,13 @@ export class PortCallOverviewService extends BaseRequest {
     }
 
     getPortCalls() {
-        let options = this.getRequestOptions();
-        let uri = this.portCallUserUrl;
-        return this.http.get(uri, options)
-            .map(res => res.json());
+        const options = this.getRequestOptions();
+        const uri = this.portCallUserUrl;
+        return this.http.get(uri, options).map(res => res.json());
     }
 
     getOverviews() {
-        let uri = this.overviewUrl;
-        return this.http.get(uri)
-            .map(res => res.json());
+        const uri = this.overviewUrl;
+        return this.http.get(uri).map(res => res.json());
     }
 }
