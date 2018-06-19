d2bf21cfa8b29d951993dec787a2b74405d5dba1
Joar K. Mauseth <jkm99412@gmail.com>
2018-06-19 15:24:01 +0200

    Corrected some test classes

--- IMOMaritimeSingleWindow.Tests/UserTestBase.cs
+++ IMOMaritimeSingleWindow.Tests/UserTestBase.cs
@@ -1,64 +1,83 @@
-﻿//using AutoMapper;
-//using IMOMaritimeSingleWindow.Data;
-//using IMOMaritimeSingleWindow.Helpers;
-//using IMOMaritimeSingleWindow.Identity.Helpers;
-//using IMOMaritimeSingleWindow.Identity.Models;
-//using IMOMaritimeSingleWindow.Identity.Stores;
-//using IMOMaritimeSingleWindow.Repositories;
-//using IMOMaritimeSingleWindow.Tests.Constants;
-//using IMOMaritimeSingleWindow.Tests.Data;
-//using IMOMaritimeSingleWindow.ViewModels.Mappings;
-//using Microsoft.AspNetCore.Identity;
-//using System.Collections.Generic;
+﻿using AutoMapper;
+using IMOMaritimeSingleWindow.Data;
+using IMOMaritimeSingleWindow.Helpers;
+using IMOMaritimeSingleWindow.Identity.Helpers;
+using IMOMaritimeSingleWindow.Identity.Models;
+using IMOMaritimeSingleWindow.Identity.Stores;
+using IMOMaritimeSingleWindow.Repositories;
+using IMOMaritimeSingleWindow.Tests.Constants;
+using IMOMaritimeSingleWindow.Tests.Data;
+using IMOMaritimeSingleWindow.ViewModels.Mappings;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
-//namespace IMOMaritimeSingleWindow.Tests
-//{
-//    public class UserTestBase
-//    {
+namespace IMOMaritimeSingleWindow.Tests
+{
+    public class UserTestBase
+    {
 
-//        protected open_ssnContext InMemoryDatabaseContext;
-//        protected readonly UnitOfWork UnitOfWork;
-//        protected readonly UserStore UserStore;
-//        public List<ApplicationUser> Users { get; }
-//        public IMapper Mapper { get; }
-//        public UserTestBase(InMemoryDatabaseTypes type, IdentityErrorDescriber identityErrorDescriber)
-//        {
-//            switch (type)
-//            {
-//                case InMemoryDatabaseTypes.USER_ROLE:
-//                    InMemoryDatabaseContext = StorageBuilder.GetInMemContextUserAndRoleData();
-//                    break;
-//                case InMemoryDatabaseTypes.USER:
-//                    break;
-//                case InMemoryDatabaseTypes.CLEAN:
-//                    InMemoryDatabaseContext = StorageBuilder.GetInMemContext();
-//                    break;
-//                default:
-//                    InMemoryDatabaseContext = StorageBuilder.GetInMemContext();
-//                    break;
-//            }
+        protected TestContext InMemoryDatabaseContext;
+        protected readonly UnitOfWork UnitOfWork;
+        protected readonly UserStore UserStore;
+        public List<ApplicationUser> Users { get; }
+        public IMapper Mapper { get; }
+        private string SeedFilePath { get; }
+        public UserTestBase(InMemoryDatabaseTypes type)
+        {
+            var factory = new ContextFactory();
+            var context = factory.CreateContext();
+            switch (type)
+            {
+                case InMemoryDatabaseTypes.USER_ROLE:
+                    SeedFilePath = "path-to-user-role-seed";
+                    InMemoryDatabaseContext = SeedContext(context);
+                    break;
+                case InMemoryDatabaseTypes.USER:
+                    SeedFilePath = "path-to-user-seed";
+                    InMemoryDatabaseContext = SeedContext(context);
+                    break;
+                case InMemoryDatabaseTypes.CLEAN:
+                    // No seeding neccessary
+                    InMemoryDatabaseContext = context;
+                    break;
+                default:
+                    InMemoryDatabaseContext = context;
+                    break;
+            }
             
-//            UnitOfWork = (UnitOfWork)StorageBuilder.GetUnitOfWork(InMemoryDatabaseContext);
-//            //var configuration = new MapperConfiguration(cfg => new IdentityEntitiesToModelsMappingProfile());
-//            var config = new MapperConfiguration(cfg =>
-//            {
-//                cfg.AddProfile<IdentityEntitiesToModelsMappingProfile>();
-//                cfg.AddProfile<ViewModelToEntityMappingProfile>();
-//            });
-//            Mapper = config.CreateMapper();
-//            var roleStore = new RoleStore(UnitOfWork, Mapper);
-//            UserStore = new UserStore(identityErrorDescriber, UnitOfWork, roleStore, new UserStoreHelper(Mapper), Mapper);
+            //var configuration = new MapperConfiguration(cfg => new IdentityEntitiesToModelsMappingProfile());
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<IdentityEntitiesToModelsMappingProfile>();
+                cfg.AddProfile<ViewModelToEntityMappingProfile>();
+            });
+            Mapper = config.CreateMapper();
+            UnitOfWork = new UnitOfWork(InMemoryDatabaseContext);
+            var roleStore = new RoleStore(UnitOfWork, Mapper);
+            
+            UserStore = new UserStore(new IdentityErrorDescriber(), UnitOfWork, roleStore, new UserStoreHelper(Mapper), Mapper);
 
-//            //Initialize the users list
-//            Users = new List<ApplicationUser>
-//            {
-//                new ApplicationUser
-//                {
-//                    GivenName = "Ola",
-//                    Email = "ola@test.no",
-//                    PasswordHash = "jh7asd6am"
-//                }
-//            };
-//        }
-//    }
-//}
+            //Initialize the users list
+            Users = new List<ApplicationUser>
+            {
+                new ApplicationUser
+                {
+                    GivenName = "Ola",
+                    Email = "ola@test.no",
+                    PasswordHash = "jh7asd6am"
+                }
+            };
+        }
+
+        private TestContext SeedContext(TestContext context)
+        {
+            String str = File.ReadAllText(SeedFilePath);
+            RawSqlString raw = new RawSqlString(str);
+            context.Database.ExecuteSqlCommand(raw);
+            return context;
+        }
+    }
+}
