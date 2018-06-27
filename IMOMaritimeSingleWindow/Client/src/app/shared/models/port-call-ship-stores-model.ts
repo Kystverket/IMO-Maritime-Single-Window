d4bbca929815fed4ce95e79c07c3a5265fbf2012
Kvistad Axel <axel.b.kvistad@gmail.com>
2018-06-27 09:00:43 +0200

    Updated client ship stores model to match server

--- IMOMaritimeSingleWindow/Client/src/app/shared/models/port-call-ship-stores-model.ts
+++ IMOMaritimeSingleWindow/Client/src/app/shared/models/port-call-ship-stores-model.ts
@@ -1,11 +1,17 @@
 import { MeasurementTypeModel } from 'app/shared/models/measurement-type-model';
+import { PortCallModel } from './port-call-model';
 
 export class PortCallShipStoresModel {
+    falShipStoresId: number;
     sequenceNumber: number;
     articleName: string;
     articleCode: number;
     quantity: number;
-    measurementType: MeasurementTypeModel;
-    locationOnBoardCode: number;
+    locationOnBoardCode: string;
     locationOnBoard: string;
+    measurementTypeId: number;
+    portCallId: number;
+    // Connected tables
+    measurementType: MeasurementTypeModel;
+    portCall: PortCallModel; // Probably won't be necessary to use this one
 }
