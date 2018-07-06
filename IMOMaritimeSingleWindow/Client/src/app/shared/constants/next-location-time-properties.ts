d1a2ba9434a569b90a3ef3a0ad286c43e0efacf8
Kvistad Axel <axel.b.kvistad@gmail.com>
2018-07-06 16:16:39 +0200

    Added info table for prev&next poc

---
+++ IMOMaritimeSingleWindow/Client/src/app/shared/constants/next-location-time-properties.ts
@@ -0,0 +1,7 @@
+export class NextLocationTimeProperties {
+    static PROPERTIES = {
+        LOCATION_NAME: { description: 'Location Name', data: null },
+        LOCATION_CODE: { description: 'Location Code', data: null },
+        ETA: { description: 'ETA', data: null },
+    };
+}
---
+++ IMOMaritimeSingleWindow/Client/src/app/shared/constants/prev-location-time-properties.ts
@@ -0,0 +1,7 @@
+export class PrevLocationTimeProperties {
+    static PROPERTIES = {
+        LOCATION_NAME: { description: 'Location Name', data: null },
+        LOCATION_CODE: { description: 'Location Code', data: null },
+        ETD: { description: 'ETD', data: null },
+    };
+}
