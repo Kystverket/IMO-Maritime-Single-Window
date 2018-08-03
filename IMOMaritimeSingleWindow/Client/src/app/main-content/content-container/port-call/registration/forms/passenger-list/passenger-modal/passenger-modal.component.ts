03bf25043056b8d4781e034d4867677ad62ce748
Unknown <cammiida@gmail.com>
2018-08-03 11:10:22 +0200

    Can show and search for countries in editModal

--- IMOMaritimeSingleWindow/Client/src/app/main-content/content-container/port-call/registration/forms/passenger-list/passenger-modal/passenger-modal.component.ts
+++ IMOMaritimeSingleWindow/Client/src/app/main-content/content-container/port-call/registration/forms/passenger-list/passenger-modal/passenger-modal.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input, Output, ViewChild, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, ViewChild, EventEmitter, ViewChildren } from '@angular/core';
 import { PersonOnBoardModel } from 'app/shared/models/person-on-board-model';
 import { NgbModal } from '../../../../../../../../../node_modules/@ng-bootstrap/ng-bootstrap';
+import { SearchCountryComponent } from 'app/shared/components/search-country/search-country.component';
 
 @Component({
   selector: 'app-passenger-modal',
@@ -42,12 +43,35 @@ export class PassengerModalComponent implements OnInit {
   }
 
   setNationality($event) {
+    console.log($event);
+    this.inputPassengerModel.nationality = $event.item;
+    this.inputPassengerModel.nationalityId = $event.item.countryId;
   }
 
   setCountryOfBirth($event) {
+    console.log($event);
+    this.inputPassengerModel.countryOfBirth = $event.item;
+    this.inputPassengerModel.countryOfBirthId = $event.item.countryId;
+  }
+
+  setIssuingNation($event) {
+    console.log($event);
+    this.inputPassengerModel.identityDocument[0].issuingNation = $event.item;
+    this.inputPassengerModel.identityDocument[0].issuingNationId = $event.item.countryId;
   }
 
   deselectNationality() {
+    this.inputPassengerModel.nationality = null;
+    this.inputPassengerModel.nationalityId = null;
   }
 
+  deselectCountryOfBirth() {
+    this.inputPassengerModel.countryOfBirth = null;
+    this.inputPassengerModel.countryOfBirthId = null;
+  }
+
+  deselectIssuingNation() {
+    this.inputPassengerModel.identityDocument[0].issuingNation = null;
+    this.inputPassengerModel.identityDocument[0].issuingNationId = null;
+  }
 }
