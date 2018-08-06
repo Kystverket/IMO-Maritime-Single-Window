6200b1ea52ff85a08bc6b3002f1a6cb6706382f4
Gard Steinsvik <gardsteinsvik@gmail.com>
2018-08-06 09:13:54 +0200

    Fixed bug that caused status not being set when giving or removing final clearance.
    I think so at least

--- IMOMaritimeSingleWindow/Client/src/app/main-content/content-container/port-call/clearance/clearance.component.ts
+++ IMOMaritimeSingleWindow/Client/src/app/main-content/content-container/port-call/clearance/clearance.component.ts
@@ -63,6 +63,7 @@ export class ClearanceComponent implements OnInit, OnDestroy {
   }
 
   saveClearance() {
+    console.log(this.clearanceModel);
     this.clearanceModel.remark = this.clearanceText;
     this.clearanceModel.cleared = this.givingClearance;
     this.portCallService.saveClearance(this.clearanceModel);
@@ -79,13 +80,17 @@ export class ClearanceComponent implements OnInit, OnDestroy {
           });
           if (allCleared) {
             this.portCallService.updatePortCallStatusCleared(this.clearanceModel.portCallId).subscribe(
-              res => console.log('Status set to cleared.'),
+              res => {
+                console.log(res);
+                console.log('Status set to cleared.');
+              },
               err => console.log(err)
             );
           }
         }
       );
     } else {
+      console.log('Setting status to AC...');
       this.portCallService.updatePortCallStatusAwaitingClearance(this.clearanceModel.portCallId).subscribe(
         res => {
           console.log(res);
