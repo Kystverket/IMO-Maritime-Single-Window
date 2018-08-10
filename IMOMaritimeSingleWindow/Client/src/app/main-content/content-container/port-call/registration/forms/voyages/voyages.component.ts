df223e9c01f0b8c12e7af8be6465685a986cadef
Gard Steinsvik <gardsteinsvik@gmail.com>
2018-08-10 15:36:29 +0200

    Moved search ship and location functionality into voyages. Moved ETA and ETD into Select Port Call section

--- IMOMaritimeSingleWindow/Client/src/app/main-content/content-container/port-call/registration/forms/voyages/voyages.component.ts
+++ IMOMaritimeSingleWindow/Client/src/app/main-content/content-container/port-call/registration/forms/voyages/voyages.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ShipProperties } from 'app/shared/constants/ship-properties';
+import { PortCallService } from 'app/shared/services/port-call.service';
+import { LocationProperties } from 'app/shared/constants/location-properties';
+import { EtaEtdDateTime } from 'app/shared/interfaces/eta-etd-date-time.interface';
+import { NgbDate } from '../../../../../../../../node_modules/@ng-bootstrap/ng-bootstrap/datepicker/ngb-date';
 
 @Component({
   selector: 'app-voyages',
@@ -9,9 +14,98 @@ export class VoyagesComponent implements OnInit {
 
   @Input() portCallId: number;
 
-  constructor() { }
+  shipFound = false;
+  shipProperties = new ShipProperties().getPropertyList();
+
+  locationFound = false;
+  locationProperties = new LocationProperties().getPropertyList();
+
+  etaEtdModel: EtaEtdDateTime = {
+    eta: null,
+    etd: null
+  };
+
+  dateSequenceError = false;
+  timeSequenceError = false;
+
+  constructor(private portCallService: PortCallService) { }
 
   ngOnInit() {
   }
 
+  onShipResult(shipResult) {
+    const twoCharCode = shipResult.shipFlagCode.country.twoCharCode.toLowerCase() || 'xx';
+    const countryFlag = twoCharCode + '.png';
+    ShipProperties.setShipData(this.shipProperties, shipResult);
+    ShipProperties.setCountry(this.shipProperties, null, countryFlag);
+
+    this.portCallService.setShipData(shipResult);
+    this.shipFound = true;
+  }
+
+  deselectShip() {
+    this.portCallService.setShipData(null);
+    this.shipFound = false;
+  }
+
+  onLocationResult(locationResult) {
+    const twoCharCode = locationResult.country.twoCharCode.toLowerCase() || 'xx';
+    const countryFlag = twoCharCode + '.png';
+    LocationProperties.setLocationData(this.locationProperties, locationResult);
+    LocationProperties.setCountry(this.locationProperties, locationResult.country.name, countryFlag);
+
+    this.portCallService.setLocationData(locationResult);
+    this.locationFound = true;
+  }
+
+  deselectLocation() {
+    this.portCallService.setLocationData(null);
+    this.locationFound = false;
+  }
+
+  onEtaResult(etaResult) {
+    this.etaEtdModel.eta = etaResult;
+    this.validateData();
+  }
+
+  onEtdResult(etdResult) {
+    this.etaEtdModel.etd = etdResult;
+    this.validateData();
+  }
+
+  private validateData() {
+    if (this.etaEtdModel.eta && this.etaEtdModel.etd) {
+      const etaDate = new NgbDate(this.etaEtdModel.eta.date.year, this.etaEtdModel.eta.date.month, this.etaEtdModel.eta.date.day);
+      const etdDate = new NgbDate(this.etaEtdModel.etd.date.year, this.etaEtdModel.etd.date.month, this.etaEtdModel.etd.date.day);
+
+      this.dateSequenceError = etdDate.before(etaDate);
+
+      if (etdDate.equals(etaDate)) {
+        this.timeSequenceError = this.etaEtdModel.eta.time.hour > this.etaEtdModel.etd.time.hour
+          || (this.etaEtdModel.eta.time.hour === this.etaEtdModel.etd.time.hour
+          && this.etaEtdModel.eta.time.minute >= this.etaEtdModel.etd.time.minute);
+      } else {
+        this.timeSequenceError = false;
+      }
+    } else {
+      this.dateSequenceError = false;
+      this.timeSequenceError = false;
+    }
+    this.persistData();
+  }
+
+  private persistData() {
+
+    if (!this.dateSequenceError && !this.timeSequenceError && this.etaEtdModel && this.etaEtdModel.eta && this.etaEtdModel.etd) {
+      const etaEtdData = {
+        eta: Object.assign(this.etaEtdModel.eta.date, this.etaEtdModel.eta.time),
+        etd: Object.assign(this.etaEtdModel.etd.date, this.etaEtdModel.etd.time)
+      };
+
+      // const formattedDate = this.portCallService.etaEtdDataFormat()
+      this.portCallService.setEtaEtdData(etaEtdData);
+    } else {
+      this.portCallService.setEtaEtdData(null);
+    }
+  }
 }
