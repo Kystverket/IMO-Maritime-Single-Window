1a36c0c072eb2420b7dcd3e0d36dcc6712d2f9ab
Unknown <cammiida@gmail.com>
2018-08-24 16:30:48 +0200

    Cleanup crewList code

--- IMOMaritimeSingleWindow/Client/src/app/main-content/content-container/port-call/registration/forms/passenger-list/passenger-list.component.ts
+++ IMOMaritimeSingleWindow/Client/src/app/main-content/content-container/port-call/registration/forms/passenger-list/passenger-list.component.ts
@@ -179,6 +179,7 @@ export class PassengerListComponent implements OnInit, OnDestroy {
     // Add
     this.passengerList.push(this.portCallPassengerModel);
     this.persistData();
+
     // Reset
     this.clearForm();
   }
