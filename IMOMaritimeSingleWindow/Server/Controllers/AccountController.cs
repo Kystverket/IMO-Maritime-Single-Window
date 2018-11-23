d016892b5120a90b6f19384c3c8cded5fd5eabd2
bardlokas <bard.lokas@norconsult.com>
2018-11-23 14:48:42 +0100

    Comment email regex

--- IMOMaritimeSingleWindow/Server/Controllers/AccountController.cs
+++ IMOMaritimeSingleWindow/Server/Controllers/AccountController.cs
@@ -359,9 +359,10 @@ namespace IMOMaritimeSingleWindow.Controllers
         [HttpGet("user/search/{searchTerm}/{amount}")]
         public async Task<IActionResult> SearchUserJson(string searchTerm, int amount)
         {
-            IQueryable<IMOMaritimeSingleWindow.Models.User> queryableUser = null; 
+            IQueryable<IMOMaritimeSingleWindow.Models.User> queryableUser = null;
 
             // check to see if the search term is an email
+            // https://emailregex.com/ 
             var isEmail = Regex.IsMatch(searchTerm,
                 @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
                 @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
