c61949793b494569cd1af5a679d35251c26c1b36
bardlokas <bard.lokas@norconsult.com>
2019-02-28 13:43:21 +0100

    Http client fix for file uploading

--- IMOMaritimeSingleWindow/Client/src/app/shared/services/file.service.ts
+++ IMOMaritimeSingleWindow/Client/src/app/shared/services/file.service.ts
@@ -1,33 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+
 @Injectable()
 export class FileService {
     private overviewUrl: string;
 
     constructor(
         private httpClient: HttpClient,
+        private http: Http,
     ) {
     }
-
     uploadPaxFile(formData: FormData, portCallId: number): Observable<any> {
         const uri = 'api/file/passengers/' + portCallId;
-        return this.httpClient.post(uri, formData);
+        return this.http.post(uri, formData);
     }
 
     uploadCrewFile(formData: FormData, portCallId: number): Observable<any> {
         const uri = 'api/file/crew/' + portCallId;
-        return this.httpClient.post(uri, formData);
+        return this.http.post(uri, formData);
     }
 
     uploadCrewAndPax(formData: FormData, portCallId: number): Observable<any> {
         const uri = 'api/file/crewpax/' + portCallId;
-        return this.httpClient.post(uri, formData);
+        return this.http.post(uri, formData);
     }
 
     uploadShipStores(formData: FormData, portCallId: number): Observable<any> {
         const uri = 'api/file/shipStores/' + portCallId;
-        return this.httpClient.post(uri, formData);
+        return this.http.post(uri, formData);
     }
 
     getClearanceCertificate(url: string) {
