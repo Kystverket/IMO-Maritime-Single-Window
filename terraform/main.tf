f2cd3711570b6eb9a8a24ea1902471617b8ff2a1
vegardrs <vegard.rist.sorheim@gmail.com>
2024-07-01 15:10:15 +0200

    resource groups etc. already exists

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,18 @@
-resource "azurerm_resource_group" "main" {
-  name     = "imo-sw-infra-rg"
-  location = var.location
-}
+# resource "azurerm_resource_group" "main" {
+#   name     = "imo-sw-infra-rg"
+#   location = var.location
+# }
 
-resource "azurerm_storage_account" "main" {
-  name                     = "imoswinfra"
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+# resource "azurerm_storage_account" "main" {
+#   name                     = "imoswinfra"
+#   resource_group_name      = azurerm_resource_group.main.name
+#   location                 = var.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
 
-resource "azurerm_storage_container" "main" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.main.name
-  container_access_type = "private"
-}
+# resource "azurerm_storage_container" "main" {
+#   name                  = "tfstate"
+#   storage_account_name  = azurerm_storage_account.main.name
+#   container_access_type = "private"
+# }
