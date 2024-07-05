89afd53d1da81ebb45f706c2dc2770a9a5b0c9a0
vegardrs <vegard.rist.sorheim@gmail.com>
2024-07-05 11:35:46 +0200

    modified:   terraform/aca.tf

--- terraform/aca.tf
+++ terraform/aca.tf
@@ -131,7 +131,7 @@ resource "azurerm_container_app" "backend" {
       }
       env {
         name  = "PGPASSWORD"
-        secret_name = "db_password_secret"
+        secret_name = "pg-password-secret"
       }
     }
   }
@@ -139,6 +139,6 @@ resource "azurerm_container_app" "backend" {
   tags = local.default_tags
 
   lifecycle {
-    ignore_changes = [template[0].container[0].image]
+    ignore_changes = [template[0].container[0].image, value]
   }
 }
