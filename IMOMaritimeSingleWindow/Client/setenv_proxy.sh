d1b870d5f3ae837c26182473c35cc9db339ec1ad
vegardrs <vegard.rist.sorheim@gmail.com>
2024-07-08 16:07:15 +0200

    added script for append env from azure to proxysettings of client

---
+++ IMOMaritimeSingleWindow/Client/setenv_proxy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# setenv_proxy.sh - Script to set environment variables in a JSON config
+
+# Path to the JSON config file
+PROXY_CONFIG_FILE="./proxy.config.json"
+
+# Write the JSON configuration
+echo '{
+  "/api": {
+    "target": "'"$BACKEND_URL"'",
+    "secure": false
+  }
+}' > $PROXY_CONFIG_FILE
+
+# Now you can add any other startup commands you need
