970d7d97e0c23a5e857652bccd542382dd6f86ce
vegardrs <vegard.rist.sorheim@gmail.com>
2024-07-08 15:23:15 +0200

    modified:   terraform/aca.tf

--- terraform/aca.tf
+++ terraform/aca.tf
@@ -56,7 +56,7 @@ resource "azurerm_container_app" "frontend" {
   template {
     container {
       name   = "frontend"
-      image  = "${data.azurerm_container_registry.acr.login_server}/client:1"
+      image  = "${data.azurerm_container_registry.acr.login_server}/client:latest"
       cpu    = 1.0
       memory = "2Gi"
       #ephemeral_storage = "5Gi"
@@ -118,7 +118,7 @@ resource "azurerm_container_app" "backend" {
   template {
     container {
       name   = "backend" 
-      image  = "${data.azurerm_container_registry.acr.login_server}/server:1"
+      image  = "${data.azurerm_container_registry.acr.login_server}/server:latest"
       cpu    = 0.25
       memory = "0.5Gi"
       env {
