9d7a09cc2030cfb8b1e8b986ee461e8d48baad96
Eduard Andrei Cristea <s-educri@bouvet.no>
2024-07-09 16:27:05 +0200

    removed nginx and added api urls in envs.ts

--- IMOMaritimeSingleWindow/Client/setenv_proxy.sh
+++ IMOMaritimeSingleWindow/Client/setenv_proxy.sh
@@ -8,9 +8,29 @@ PROXY_CONFIG_FILE="./proxy.config.json"
 echo '{
   "/api": {
     "target": "'"$BACKEND_URL"'",
-    "secure": true
+    "secure": true,
     "changeOrigin":true
   }
 }' > $PROXY_CONFIG_FILE
 
-# Now you can add any other startup commands you need
+
+
+# Path to the environment files
+DEV_ENV_FILE="./src/environments/environment.ts"
+PROD_ENV_FILE="./src/environments/environment.prod.ts"
+
+
+# Backend URL
+
+# Update the development environment file
+echo "export const environment = {
+  production: false,
+  apiUrl: '$BACKEND_URL'
+};" > $DEV_ENV_FILE
+
+
+# Update the production environment file
+echo "export const environment = {
+  production: true,
+  apiUrl: '$BACKEND_URL'
+};" > $PROD_ENV_FILE
