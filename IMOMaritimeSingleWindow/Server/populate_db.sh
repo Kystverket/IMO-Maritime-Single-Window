27c7e5496d234e1ffa98eb13159b9c09375d118f
vegardrs <vegard.rist.sorheim@gmail.com>
2024-07-10 13:42:04 +0200

    modified:   IMOMaritimeSingleWindow/Server/populate_db.sh

--- IMOMaritimeSingleWindow/Server/populate_db.sh
+++ IMOMaritimeSingleWindow/Server/populate_db.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Export
-export PGHOST = "imo-dev-psqlflexibleserver-1.postgres.database.azure.com"
-export PGUSER = "postgres"
-export PGDATABASE = "db-imo-msw-dev-1"
-export PGPORT = 5432
+export PGHOST="imo-dev-psqlflexibleserver-1.postgres.database.azure.com"
+export PGUSER="postgres"
+export PGDATABASE="db-imo-msw-dev-1"
+export PGPORT=5432
 
 echo "Eduard Begin"
 # Count the tables in the 'public' schema
 TABLE_COUNT=$(psql -h $PGHOST -U $PGUSER -d $PGDATABASE -p $PGPORT -t -c "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'public';" 2>&1)
-echo $TABLE_COUNT
+echo "$TABLE_COUNT"
 echo "Finished"
 # Check if there are any tables
 if [ "$TABLE_COUNT" -ne 0 ]; then
