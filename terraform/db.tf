b18af1483181161de3a4be3c057f291b20af0b9e
vegardrs <vegard.rist.sorheim@gmail.com>
2024-07-10 09:21:39 +0200

    remove server and db

--- terraform/db.tf
+++ terraform/db.tf
@@ -35,35 +35,35 @@ resource "azurerm_key_vault_secret" "db_password" {
   key_vault_id = azurerm_key_vault.imo_dev_app.id
 }
 
-resource "azurerm_postgresql_flexible_server" "imo_dev_app" {
-  name                   = "imo-dev-psqlflexibleserver-1"
-  resource_group_name    = data.azurerm_resource_group.imo_dev_app.name
-  location               = var.location
-  version                = "13"
-  administrator_login    = "postgres"
-  administrator_password = azurerm_key_vault_secret.db_password.value
-  storage_mb             = 32768
-  sku_name               = "B_Standard_B1ms"
+# resource "azurerm_postgresql_flexible_server" "imo_dev_app" {
+#   name                   = "imo-dev-psqlflexibleserver-1"
+#   resource_group_name    = data.azurerm_resource_group.imo_dev_app.name
+#   location               = var.location
+#   version                = "13"
+#   administrator_login    = "postgres"
+#   administrator_password = azurerm_key_vault_secret.db_password.value
+#   storage_mb             = 32768
+#   sku_name               = "B_Standard_B1ms"
 
-  lifecycle {
-    ignore_changes = [
-      zone,
-      high_availability[0].standby_availability_zone,
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [
+#       zone,
+#       high_availability[0].standby_availability_zone,
+#     ]
+#   }
+# }
 
-resource "azurerm_postgresql_flexible_server_database" "imo_dev_app" {
-  name      = "db-imo-msw-dev-1"
-  server_id = azurerm_postgresql_flexible_server.imo_dev_app.id
-  collation = "en_US.utf8"
-  charset   = "utf8"
+# resource "azurerm_postgresql_flexible_server_database" "imo_dev_app" {
+#   name      = "db-imo-msw-dev-1"
+#   server_id = azurerm_postgresql_flexible_server.imo_dev_app.id
+#   collation = "en_US.utf8"
+#   charset   = "utf8"
 
-  # prevent the possibility of accidental data loss
-  # lifecycle {
-  #   prevent_destroy = true
-  # }
-}
+#   # prevent the possibility of accidental data loss
+#   # lifecycle {
+#   #   prevent_destroy = true
+#   # }
+# }
 
 resource "azurerm_postgresql_flexible_server_firewall_rule" "imo_dev_app" {
   name             = "db-fw"
