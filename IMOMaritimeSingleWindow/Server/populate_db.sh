9cd95bdca332765f86a3651409d8fede807b532f
vegardrs <vegard.rist.sorheim@gmail.com>
2024-07-10 13:28:05 +0200

    modified:   IMOMaritimeSingleWindow/Server/populate_db.sh

--- IMOMaritimeSingleWindow/Server/populate_db.sh
+++ IMOMaritimeSingleWindow/Server/populate_db.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+echo "Eduard Begin"
 # Count the tables in the 'public' schema
 TABLE_COUNT=$(psql -h $PGHOST -U $PGUSER -d $PGDATABASE -p $PGPORT -t -c "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'public';" 2>&1)
 echo $TABLE_COUNT
+echo "Finished"
 # Check if there are any tables
 if [ "$TABLE_COUNT" -ne 0 ]; then
     echo "Database is already populated. Exiting script."
