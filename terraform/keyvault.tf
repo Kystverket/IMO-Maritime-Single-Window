b0fb79304ac775c4bbb0a86e4ede29a79635def8
vegardrs <vegard.rist.sorheim@gmail.com>
2024-07-12 14:43:45 +0200

    modified:   terraform/access.tf 	modified:   terraform/keyvault.tf

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -13,11 +13,28 @@ resource "azurerm_key_vault" "imo_dev_app" {
   soft_delete_retention_days  = 7
   purge_protection_enabled    = true
   sku_name                    = "standard"
-  enable_rbac_authorization   = true
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
 
-  depends_on = [
-    azurerm_role_assignment.devops_key_vault
-  ]
+    key_permissions = ["Get"]
+
+    secret_permissions = ["Get", "Set", "Delete"]
+
+    storage_permissions = ["Get"]
+  }
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = azurerm_user_assigned_identity.imo_dev_app.principal_id
+
+    secret_permissions =["Get", "Set", "Delete"]
+  }
+
+#   enable_rbac_authorization   = true
+
+#   depends_on = [
+#     azurerm_role_assignment.devops_key_vault
+#   ]
 }
 
 resource "azurerm_key_vault_secret" "db_password" {
