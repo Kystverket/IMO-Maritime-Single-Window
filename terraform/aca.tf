fd2efde270e2c0bcdf1ba548ba97b8bc2e6bc843
vegardrs <vegard.rist.sorheim@gmail.com>
2024-07-12 12:49:10 +0200

    test backend restriction

--- terraform/aca.tf
+++ terraform/aca.tf
@@ -29,7 +29,7 @@ resource "azurerm_container_app" "backend" {
   }
 
   ingress {
-    external_enabled           = true
+    external_enabled           = false
     target_port                = 5000
     allow_insecure_connections = false
     traffic_weight {
