221d783180d7f96853c27de9c811a44a8b80b76c
vegardrs <vegard.rist.sorheim@gmail.com>
2024-07-12 13:38:06 +0200

    cleaning

--- terraform/aca.tf
+++ terraform/aca.tf
@@ -1,3 +1,20 @@
+locals {
+  stack = "${var.app}-${var.env}"
+
+  default_tags = {
+    environment = var.env
+    app         = var.app
+  }
+
+}
+
+resource "azurerm_log_analytics_workspace" "imo_dev_app" {
+  name                = "log-${local.stack}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.imo_dev_app.name
+  tags = local.default_tags
+}
+
 resource "azurerm_container_app_environment" "imo_dev_app" {
   name                       = "cae-${local.stack}"
   location                   = var.location
