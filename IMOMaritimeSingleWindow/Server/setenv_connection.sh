3784776585ad6b5c43c54bc5dd5a9c8770b08639
vegardrs <vegard.rist.sorheim@gmail.com>
2024-07-17 16:22:19 +0200

    new image

--- IMOMaritimeSingleWindow/Server/setenv_connection.sh
+++ IMOMaritimeSingleWindow/Server/setenv_connection.sh
@@ -4,7 +4,7 @@ APP_CONFIG_FILE="./appsettings.json"
 # Update the JSON configuration file
 echo '{
   "ConnectionStrings": {
-    "OpenSSN": "User ID=postgres;Password='"$PGPASSWORD"';Host='"$PGHOST"';Port='"$PGPORT"';Database='"$PGDATABASE"';SSL Mode=Require;",
+    "OpenSSN": "User ID=postgres;Password='"$PGPASSWORD"';Host='"$PGHOST"';Port='"$PGPORT"';Database='"$PGDATABASE"';SSL Mode=Require;Trust Server Certificate=true;",
     "UserDatabase": ""
   },
   "AppSettings": {
@@ -21,7 +21,7 @@ APP_DEFAULT_CONFIG_FILE="./appsettings.default.json"
 
 echo '{
   "ConnectionStrings": {
-    "OpenSSN": "User ID=postgres;Password='"$PGPASSWORD"';Host='"$PGHOST"';Port='"$PGPORT"';Database='"$PGDATABASE"';SSL Mode=Require;",
+    "OpenSSN": "User ID=postgres;Password='"$PGPASSWORD"';Host='"$PGHOST"';Port='"$PGPORT"';Database='"$PGDATABASE"';SSL Mode=Require;Trust Server Certificate=true;",
     "UserDatabase": ""
   },
   "AppSettings": {
