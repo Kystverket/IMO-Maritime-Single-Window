c3b1a7788dbeaa9f028c7004cadb2781bbf50e79
vegardrs <vegard.rist.sorheim@gmail.com>
2024-07-22 15:08:37 +0200

    Can now remove the .env.backend file and simplify the yaml command. Remove unecessary key vault feature.

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,12 +18,7 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-  }
+  features {}
 }
 
 resource "azurerm_resource_group" "imo_app" {
