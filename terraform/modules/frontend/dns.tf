079d15ac9b84159abcf6b6af8879b1bbab3c2327
Rødal, Mads Dyrlid <mads.rodal@bouvet.no>
2024-09-11 12:00:16 +0200

    fix count for dns

--- terraform/modules/frontend/dns.tf
+++ terraform/modules/frontend/dns.tf
@@ -1,14 +1,17 @@
 // Specificly created for imo-msw preview environment
 // Can be removed or altered for other applications
+locals {
+  add_frontend_dns = var.app == "imo-msw" && var.env == "dev-preview" ? 1 : 0
+}
 
 data "azurerm_dns_zone" "dns_zone" {
-  count               = var.app == "imo-msw" && var.env == "dev-preview"
+  count               = local.add_frontend_dns
   name                = "imo-msw-dev.kystverket.cloud"
   resource_group_name = "rg-dns"
 }
 
 resource "azurerm_dns_a_record" "frontend" {
-  count               = var.app == "imo-msw" && var.env == "dev-preview"
+  count               = local.add_frontend_dns
   name                = "@"
   zone_name           = data.azurerm_dns_zone.dns_zone.name
   resource_group_name = var.resource_group_name
@@ -17,7 +20,7 @@ resource "azurerm_dns_a_record" "frontend" {
 }
 
 resource "azurerm_dns_txt_record" "frontend" {
-  count               = var.app == "imo-msw" && var.env == "dev-preview"
+  count               = local.add_frontend_dns
   name                = "asuid"
   zone_name           = data.azurerm_dns_zone.dns_zone.name
   resource_group_name = var.resource_group_name
@@ -29,7 +32,7 @@ resource "azurerm_dns_txt_record" "frontend" {
 }
 
 resource "azurerm_container_app_custom_domain" "frontend" {
-  count            = var.app == "imo-msw" && var.env == "dev-preview"
+  count            = local.add_frontend_dns
   name             = trimprefix(azurerm_dns_txt_record.frontend.fqdn, "asuid.")
   container_app_id = azurerm_container_app.frontend.id
 
