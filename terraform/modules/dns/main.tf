904e87ba37711af04d0862fa3e901f54bb67eabd
Rødal, Mads Dyrlid <mads.rodal@bouvet.no>
2024-09-12 10:14:10 +0200

    add null provider to dns module

--- terraform/modules/dns/main.tf
+++ terraform/modules/dns/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+    }
+  }
+}
+
 data "azurerm_dns_zone" "dns_zone" {
   name                = var.dns_zone_name
   resource_group_name = var.dns_resource_group_name
